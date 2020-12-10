b141967789ee763811557794b81b47a660de7dda
daniel4916x <thalmda1@students.zhaw.ch>
2020-12-10 18:18:42 +0000

    test

--- src/app/gui-model/guimodel.ts
+++ src/app/gui-model/guimodel.ts
@@ -6,7 +6,7 @@ export class GuiModel {
 
     private _guiModel = {
         "application": {
-            "title": "Daniels Engineering Friend Tracker",
+            "title": "Daniels Engineering Friend Tracker 2",
             "formList": [
                 {
                     "id": "FriendForm",
