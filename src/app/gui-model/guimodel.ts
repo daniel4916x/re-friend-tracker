08130efc8bc5c81926566d383fc15e36639754bc
daniel4916x <thalmda1@students.zhaw.ch>
2020-12-12 16:56:11 +0000

    gg

--- src/app/gui-model/guimodel.ts
+++ src/app/gui-model/guimodel.ts
@@ -298,6 +298,7 @@ export class GuiModel {
                         {
                             "type": "newButton",
                             "name": "EditFriend",
+                            "search": true,
                             "icon": "fa-user",
                             "color": "green",
                             "form": {
